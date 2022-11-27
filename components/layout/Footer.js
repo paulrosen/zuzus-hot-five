75228eb687d6741f556e2a12de0aa4a0a878b627
Paul Rosen <paul@paulrosen.net>
2022-11-27 11:31:57 -0500

    simplify footer: remove soc.media; make the link an `<a>`; hide it on contact page.

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,38 +1,18 @@
-import { Button, Grid, Typography } from "@mui/material";
-import { Box, Container } from "@mui/system";
 import Link from "next/link";
 import React from "react";
-import SocialMediaIcons from "../general/SocialMediaIcons";
-
-const socialMedia = [
-    { media: "twitter", url: "https://google.com" },
-    { media: "instagram", url: "https://google.com" },
-    { media: "facebook", url: "https://google.com" },
-];
-
-const contactInfo = [
-    { type: "telephone", value: "555-555-5555" },
-    { type: "email", value: "hello@artistName.com" },
-];
+import { useRouter } from "next/router";
 
 const Footer = () => {
+    const router = useRouter();
+    const currentPage = router.pathname.split("/")[1];
     return (
-        <Container maxWidth="xl">
-            <Grid container sx={{ padding: "1rem 0" }}>
-                <Grid item xs={6}>
-                    <SocialMediaIcons color="secondary" fontSize="25px" />
-                </Grid>
-                <Grid item xs={6}>
-                    <Box sx={{ display: "flex", justifyContent: "end" }}>
-                        <Link href="/contact">
-                            <Button variant="outlined" color="secondary">
-                                contact
-                            </Button>
-                        </Link>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Container>
+        <footer data-page={currentPage}>
+            <Link href="/contact">
+                <a data-contact variant="outlined" color="secondary">
+                    contact
+                </a>
+            </Link>
+        </footer>
     );
 };
 
